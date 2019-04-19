3044a7efe816afc965a5b88420b62c68d180c70c
SteveMustanski <steve.mustanski@gmail.com>
2019-04-19 14:56:42 -0400

    passed in props to component

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -5,10 +5,15 @@ class Message extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello Everyone</h1>
+        <h1>Hello Everyone!</h1>
+        <p style={{ color: this.props.color }}>{this.props.msg}</p>
+        <p>I will check back in {this.props.minutes} minutes.</p>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'));
+ReactDOM.render(
+  <Message color='blue' msg='How are you?' minutes={5} />,
+  document.getElementById('root'),
+);
