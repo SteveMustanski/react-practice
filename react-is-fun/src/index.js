e050adaed493dcb30a6205a6dad08a375f8e5f40
SteveMustanski <steve.mustanski@gmail.com>
2019-04-19 15:20:09 -0400

    convered DevDayCounter to functional component

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-class Message extends React.Component {
-  render() {
-    return (
+let hundredCodeDays = {
+  total: 66,
+  react: 40,
+  js: 16,
+  node: 10,
+  goal: 100,
+};
+
+const getPercent = decimal => {
+  return decimal * 100 + '%';
+};
+
+const getGoalProgress = (total, goal) => {
+  return getPercent(total / goal);
+};
+
+const DevDayCounter = ({ total, react, js, node, goal }) => {
+  return (
+    <section>
+      <h1>Hundred Days of Code</h1>
       <div>
-        <h1>Hello Everyone!</h1>
-        <p style={{ color: this.props.color }}>{this.props.msg}</p>
-        <p>I will check back in {this.props.minutes} minutes.</p>
+        <p>Total Days: {total}</p>
       </div>
-    );
-  }
-}
+      <div>
+        <p>Total React Days: {react}</p>
+      </div>
+      <div>
+        <p>Total JS Days: {js}</p>
+      </div>
+      <div>
+        <p>Total Node Days: {node}</p>
+      </div>
+      <div>
+        <p>Goal Progress: {getGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  );
+};
 
-ReactDOM.render(
-  <Message color='blue' msg='How are you?' minutes={5} />,
+render(
+  <DevDayCounter
+    total={hundredCodeDays.total}
+    react={hundredCodeDays.react}
+    js={hundredCodeDays.js}
+    node={hundredCodeDays.node}
+    goal={hundredCodeDays.goal}
+  />,
   document.getElementById('root'),
 );
