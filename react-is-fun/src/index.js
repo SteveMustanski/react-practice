e8a1d5740ffaf9442206d6b986d4e0243b500722
SteveMustanski <steve.mustanski@gmail.com>
2019-04-20 15:16:41 -0400

    modified/added library and book components

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -1,52 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-let hundredCodeDays = {
-  total: 66,
-  react: 40,
-  js: 16,
-  node: 10,
-  goal: 100,
-};
-
-const getPercent = decimal => {
-  return decimal * 100 + '%';
-};
-
-const getGoalProgress = (total, goal) => {
-  return getPercent(total / goal);
-};
-
-const DevDayCounter = ({ total, react, js, node, goal }) => {
+const Book = ({ title, author, pages }) => {
   return (
     <section>
-      <h1>Hundred Days of Code</h1>
-      <div>
-        <p>Total Days: {total}</p>
-      </div>
-      <div>
-        <p>Total React Days: {react}</p>
-      </div>
-      <div>
-        <p>Total JS Days: {js}</p>
-      </div>
-      <div>
-        <p>Total Node Days: {node}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {getGoalProgress(total, goal)}</p>
-      </div>
+      <h1>{title}</h1>
+      <p>by {author}</p>
+      <p>pages: {pages}</p>
     </section>
   );
 };
 
-render(
-  <DevDayCounter
-    total={hundredCodeDays.total}
-    react={hundredCodeDays.react}
-    js={hundredCodeDays.js}
-    node={hundredCodeDays.node}
-    goal={hundredCodeDays.goal}
-  />,
-  document.getElementById('root'),
-);
+const Library = () => {
+  return <Book title='Atlas Shrugged' author='Ayn Rand' pages={1200} />;
+};
+
+render(<Library />, document.getElementById('root'));
